de0a1c3467c387e36674de9d3ea49bd1b0644345
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-06-21 18:32:41 +0200

    Exercice 3 Module 1 TP1 done

--- Module 1/Week 1/LEPL1402_MakeMistakeToUnderstandThem/src/main/java/Except.java
+++ Module 1/Week 1/LEPL1402_MakeMistakeToUnderstandThem/src/main/java/Except.java
@@ -6,15 +6,26 @@ public class Except{
     
     
     public static void outof(){
-        
+        int[] array = new int[]{1,2,3};
+        // can't acces index 3 bc array on lenght 3
+        System.out.println(array[3]);
     }
     
     public static void concurr(){
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for (int i : list){
+            if(i == 1){
+                list.remove(i);
+            }
+        }
     }
     
     public static void nullpointer(){
-        
+        int[] array = null;
+        int a = array.length;
     }
     
     
