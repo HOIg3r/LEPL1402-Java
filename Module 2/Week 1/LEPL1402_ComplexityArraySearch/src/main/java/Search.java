a795f82e2af7fdacdcdddf559fdb3879a68caf86
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-07-09 18:01:03 +0200

    Exercice 1 Module 2 TP3 Done

--- Module 2/Week 1/LEPL1402_ComplexityArraySearch/src/main/java/Search.java
+++ Module 2/Week 1/LEPL1402_ComplexityArraySearch/src/main/java/Search.java
@@ -1,12 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Search {
 
     /**
+     * [1,2,3,4,5,6]
      *
-     * @param tab
-     * @return
+     * @param tab is an ordered array of int.
+     * @return index of elem or -1
      */
-    public static int search(int[] tab, int elem){
-        //TODO
+    public static int search(int[] tab, int elem) {
+        int first = 0;
+        int last = tab.length - 1;
+
+        while (first <= last) {
+            int mid = (first + last) / 2;
+            if (elem == tab[mid]){
+                return mid;
+            } else if (elem < tab[mid]) {
+                last = mid - 1;
+            } else if (elem > tab[mid]) {
+                first = mid + 1;
+            }
+            else return - 1;
+        }
+        return -1;
     }
 }
