bdb35b1da9c7d9458b41f07f8ebc1314debfb872
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-07-15 13:55:10 +0200

    fibonnaci done

--- Module 2/Week 2/LEPL1402_Fibonacci/src/main/java/Fibonacci.java
+++ Module 2/Week 2/LEPL1402_Fibonacci/src/main/java/Fibonacci.java
@@ -2,12 +2,32 @@ public class Fibonacci {
 
 
     public static int fiboRecursive(int index){
-        //TODO HERE CODE HERE
+        if (index == 0){
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        }
+        else {
+            return fiboRecursive(index - 1) + fiboRecursive(index - 2);
+        }
     }
 
 
     public static int fiboIterative(int index){
-        //TODO YOUR CODE HERE
+        if (index == 0){
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        }
+
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < index; i++) {
+            int c = a;
+            a = b;
+            b = c + b;
+        }
+        return a;
     }
 
 }
