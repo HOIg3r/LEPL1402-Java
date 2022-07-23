fac5d618df82c56720dc8e52a8a46fa42e60e7b7
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-07-23 09:43:08 +0200

    MyArrayList Done

--- Module 2/Week 2/LEPL1402_MyArrayList/src/main/java/MyArrayList.java
+++ Module 2/Week 2/LEPL1402_MyArrayList/src/main/java/MyArrayList.java
@@ -1,42 +1,70 @@
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MyArrayList<Item> implements Iterable<Item> {
 
-    private Object [] list;
+    private Object[] list;
     private int size;
 
 
-    public MyArrayList(int initSize){
-        // YOUR CODE HERE
+    public MyArrayList(int initSize) {
+        if(initSize <= 1){
+            throw new IndexOutOfBoundsException("Must use a size higher than 1");
+        }
+        list = new Object[initSize];
+        size = 0;
+    }
+
+
+    public void resize() {
+        Object[] newArray = new Object[size() * 2];
+        for (int i = 0; i < list.length; i++) {
+            newArray[i] = list[i];
+        }
+        list = newArray;
+    }
+
+    /*
+     * Checks if 'list' needs to be resized then add the element at the end
+     * of the list.
+     */
+    public void enqueue(Item item) {
+        if (list.length == size()) {
+            resize();
+        }
+        list[size()] = item;
+        size++;
     }
 
 
     /*
-    * Checks if 'list' needs to be resized then add the element at the end 
-    * of the list.
-    */
-    public void enqueue(Item item){
-        // YOUR CODE HERE
+     * Removes the element at the specified position in this list.
+     * Returns the element that was removed from the list. You dont need to
+     * resize when removing an element.
+     */
+    public Item remove(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Item tmp = (Item) list[index];
+        for (int i = index + 1; i < size() - 1; i++) {
+            list[i - 1] = list[i];
+        }
+
+        list[size() - 1] = null;
+        size--;
+        return tmp;
     }
 
 
-    /*
-    * Removes the element at the specified position in this list.
-    * Returns the element that was removed from the list. You dont need to 
-    * resize when removing an element.
-    */
-    public Item remove(int index){
-        // YOUR CODE HERE
-    }
-
-
-    public int size(){
+    public int size() {
         return this.size;
     }
-    
-    
-    public Object [] getList(){
+
+
+    public Object[] getList() {
         return this.list;
     }
 
@@ -48,6 +76,27 @@ public class MyArrayList<Item> implements Iterable<Item> {
 
 
     private class MyArrayListIterator implements Iterator<Item> {
+
+        private int index = 0;
+        private int size = MyArrayList.this.size();
+
+        @Override
+        public boolean hasNext() {
+            if (size != MyArrayList.this.size) {
+                throw new ConcurrentModificationException();
+            }
+            return index < size;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            index++;
+            return (Item) list[index - 1];
+
+        }
         // YOUR CODE HERE
     }
 
