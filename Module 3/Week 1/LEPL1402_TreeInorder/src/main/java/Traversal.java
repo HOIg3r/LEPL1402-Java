983c77e4efc922f9bd8619c22675c9e6e56ce258
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-07-23 13:43:44 +0200

    Binary tree travel Done

--- Module 3/Week 1/LEPL1402_TreeInorder/src/main/java/Traversal.java
+++ Module 3/Week 1/LEPL1402_TreeInorder/src/main/java/Traversal.java
@@ -4,12 +4,32 @@ import java.util.Stack; // this should give you a hint for the iterative version
 public class Traversal {
 
     public static void recursiveInorder(Node root, List<Integer> res) {
-        // YOUR CODE HERE
+        if (root.left != null) {
+            recursiveInorder(root.left, res);
+        }
+
+        res.add(root.val);
+
+        if (root.right != null) {
+            recursiveInorder(root.right, res);
+        }
     }
 
 
-    public static void iterativeInorder(Node root, List<Integer> res){
-        // YOUR CODE HERE
+    public static void iterativeInorder(Node root, List<Integer> res) {
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        while (!stack.isEmpty() || current != null){
+            if(current != null){
+                stack.push(current);
+                current = current.left;
+            }else{
+                current = stack.pop();
+                res.add(current.val);
+                current = current.right;
+            }
+        }
     }
 
 }
