f4ad0e442b670d133150a5271ee4c4e88f230313
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-08-10 21:04:27 +0200

    add exercice 1 Module 5 Week 2

--- Module 5/Week 1/LEPL1402_Generics3/src/main/java/Evaluator.java
+++ Module 5/Week 1/LEPL1402_Generics3/src/main/java/Evaluator.java
@@ -6,19 +6,42 @@ import java.util.function.Function;
 public class Evaluator {
 
     public BiFunction<Boolean, Boolean, Boolean> xor_gate() {
-        // TODO BY STUDENT
+        return (a,b) -> {
+            if (a) {
+                if(b){
+                    return false;
+                }
+                return true;
+            }
+            return b;
+        };
     }
 
     public BiFunction<Boolean, Boolean, Boolean> or_gate() {
-        // TODO BY STUDENT
+        return (a,b) -> {
+            if (a) {
+                return true;
+            }
+            return b;
+        };
     }
 
     public BiFunction<Boolean, Boolean, Boolean> and_gate() {
-        // TODO BY STUDENT
+        return (a,b) -> {
+            if (a) {
+                return b;
+            }
+            return false;
+        };
     }
 
     public Function<Boolean, Boolean> not_gate() {
-        // TODO BY STUDENT
+        return (a) -> {
+            if (a) {
+                return false;
+            }
+            return true;
+        };
     }
 
     // Should return a map containing the computation's results (use HashMap)
@@ -26,7 +49,10 @@ public class Evaluator {
     // TODO WARNING : ONLY USE the previously defined methods to compute the result
     // (INGInious will prevent you from cheating by directly invoking logical operators)
     public Map<String, Boolean> evaluate_circuit(Boolean a, Boolean b, Boolean carry_in) {
-        // TODO BY STUDENT
+        Map<String,Boolean> map = new HashMap<>();
+        map.put("SUM",xor_gate().apply(xor_gate().apply(a,b),carry_in));
+        map.put("carry_out",or_gate().apply(and_gate().apply(carry_in,xor_gate().apply(a,b)), and_gate().apply(a,b)));
+        return map;
     }
 
 }
