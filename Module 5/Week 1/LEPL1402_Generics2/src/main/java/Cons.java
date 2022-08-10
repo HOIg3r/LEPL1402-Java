900e4343543180b98dd1aa976db022cc7704b29d
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-08-10 20:44:55 +0200

    add exercice 4 Module 5 Week 1

--- Module 5/Week 1/LEPL1402_Generics2/src/main/java/Cons.java
+++ Module 5/Week 1/LEPL1402_Generics2/src/main/java/Cons.java
@@ -7,18 +7,37 @@ public class Cons<E> {
     // The next element, null if there is none
     public Cons<E> next;
 
-    // Returns a new Cons by applying Function function on all elements
-    public <R> Cons <R> map(Function <E,R> function) {
-        //TODO
-    }
-    // Returns a new Cons containing all elements that match Predicate predicate
-    public Cons <E> filter(Predicate <E> predicate) {
-        //TODO
-    }
-
     // Constructor
     public Cons(E v, Cons<E> next) {
         this.v = v;
         this.next = next;
     }
+
+    // Returns a new Cons by applying Function function on all elements
+    public <R> Cons <R> map(Function <E,R> function) {
+        Cons<R> tmp = new Cons<>(function.apply(this.v), null);
+        if (next != null){
+            tmp.next = this.next.map(function);
+        }
+        return tmp;
+    }
+    // Returns a new Cons containing all elements that match Predicate predicate
+    public Cons <E> filter(Predicate <E> predicate) {
+        Cons<E> tmp = new Cons<E>(null,null);
+        Cons<E> head = this;
+
+        while(head != null){
+            if(predicate.test(head.v)){
+                tmp.v = head.v;
+                if(head.next != null){
+                    tmp.next = head.next.filter(predicate);
+                }
+                return tmp;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+
 }
