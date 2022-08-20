8c03dd82309c6f839e609acbe7fa5edc4297f449
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-08-20 20:02:49 +0200

    add exercice 3 Module 6 Week 1

--- Module 6/Week 1/LEPL1402_ThreadsIntroduction/src/main/java/Launcher.java
+++ Module 6/Week 1/LEPL1402_ThreadsIntroduction/src/main/java/Launcher.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Launcher {
 
     /*
@@ -7,6 +11,25 @@ public class Launcher {
      * "t" array.
      */
     public static Counter[] init(Thread[] t){
-      //TODO YOUR CODE HERE
+        Counter[] output = new Counter[t.length];
+        for (int i = 0; i < t.length; i++) {
+            output[i] = new Counter();
+            t[i] = new Thread(output[i],String.valueOf(i));
+            t[i].start();
+        }
+
+        for (Thread thread: t) {
+            try {
+                thread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Thread[] t = new Thread[2];
+        System.out.println(init(t)[0].getCount());
     }
 }
